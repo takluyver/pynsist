24815c01866bda4b6c2a6fe5586239aaa969bc36
Thomas Kluyver <takowl@gmail.com>
2014-04-02 16:34:47 -0700

    Fix tkinter example

--- examples/tkinter/example.py
+++ examples/tkinter/example.py
@@ -1,11 +1,9 @@
 from tkinter import *
-import foopkg
 
 root = Tk()
 root.title("Python Example App")
 t = Text(root)
 t.insert(END, "Type stuff here.")
-t.insert(END, foopkg.strvar)
 t.pack()
 
 w = Label(root, text="Hello, world!")
