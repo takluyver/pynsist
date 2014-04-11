b4ca3ef604f20697fba3acfa137b39cfd47f2cf5
Thomas Kluyver <takowl@gmail.com>
2014-04-11 12:36:56 -0700

    Add script to build all examples

---
+++ examples/build_all_examples.py
@@ -0,0 +1,14 @@
+import os
+import nsist
+
+example_cfgs = [
+    'console/installer.cfg',
+    'pyqt/installer.cfg',
+    'tkinter/installer.cfg',
+]
+
+examples_dir = os.path.dirname(os.path.abspath(__file__))
+
+for example_cfg in example_cfgs:
+    os.chdir(examples_dir)
+    nsist.main([example_cfg])
