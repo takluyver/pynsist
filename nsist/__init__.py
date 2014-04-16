af4d3ed576c1562872bd6d298ce9a75f28c06ed3
Thomas Kluyver <takowl@gmail.com>
2014-04-16 15:53:06 -0700

    Better script template
    Closes gh-7

--- nsist/__init__.py
+++ nsist/__init__.py
@@ -79,8 +79,19 @@ def fetch_pylauncher(bitness=DEFAULT_BITNESS, destination=DEFAULT_BUILD_DIR):
     urlretrieve(url, target)
 
 SCRIPT_TEMPLATE = """#!python{qualifier}
-import sys
-sys.path.insert(0, 'pkgs')
+import sys, os
+scriptdir, script = os.path.split(__file__)
+pkgdir = os.path.join(scriptdir, 'pkgs')
+sys.path.insert(0, pkgdir)
+os.environ['PYTHONPATH'] = pkgdir + os.pathsep + os.environ.get('PYTHONPATH', '')
+
+def excepthook(etype, value, tb):
+    "Write unhandled exceptions to a file rather than exiting silently."
+    import traceback
+    with open(os.path.join(scriptdir, script+'.log'), 'w') as f:
+        traceback.print_exception(etype, value, tb, file=f)
+sys.excepthook = excepthook
+
 from {module} import {func}
 {func}()
 """
