4e2813af9d510b458e0fb0ea81025de51b9c1b5c
Thomas Kluyver <takowl@gmail.com>
2015-02-20 16:07:27 -0800

    Rework use of application log file
    - Running under pythonw (no console), redirect stdout/stderr to a file
    to avoid the programming blocking when it tries to write to them.
    - Running in the console, only print the traceback to the file on an
    uncaught exception.
    - Also print exception to stderr as normal if there's a console
    
    Closes gh-36

--- nsist/__init__.py
+++ nsist/__init__.py
@@ -161,14 +161,23 @@ pkgdir = os.path.join(scriptdir, 'pkgs')
 sys.path.insert(0, pkgdir)
 os.environ['PYTHONPATH'] = pkgdir + os.pathsep + os.environ.get('PYTHONPATH', '')
 
-appdata = os.environ.get('APPDATA', None)
-def excepthook(etype, value, tb):
-    "Write unhandled exceptions to a file rather than exiting silently."
-    import traceback
-    with open(os.path.join(appdata, script+'.log'), 'w') as f:
-        traceback.print_exception(etype, value, tb, file=f)
+# APPDATA should always be set, but in case it isn't, try user home
+# If none of APPDATA, HOME, USERPROFILE or HOMEPATH are set, this will fail.
+appdata = os.environ.get('APPDATA', None) or os.path.expanduser('~')
 
-if appdata:
+if 'pythonw' in sys.executable:
+    # Running with no console - send all stdstream output to a file.
+    kw = {{'errors': 'replace'}} if (sys.version_info[0] >= 3) else {{}}
+    sys.stdout = sys.stderr = open(os.path.join(appdata, script+'.log'), 'w', **kw)
+else:
+    # In a console. But if the console was started just for this program, it
+    # will close as soon as we exit, so write the traceback to a file as well.
+    def excepthook(etype, value, tb):
+        "Write unhandled exceptions to a file and to stderr."
+        import traceback
+        traceback.print_exception(etype, value, tb)
+        with open(os.path.join(appdata, script+'.log'), 'w') as f:
+            traceback.print_exception(etype, value, tb, file=f)
     sys.excepthook = excepthook
 
 {extra_preamble}
