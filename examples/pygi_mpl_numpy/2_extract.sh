1aab956bbd321bec8389b5df3832269e97e649cc
tobias47n9e <tobias@localhost.localdomain>
2015-03-29 19:15:07 +0200

    improved example-script's handling of the gnome dependencies

--- examples/pygi_mpl_numpy/2_extract.sh
+++ examples/pygi_mpl_numpy/2_extract.sh
@@ -1,4 +1,4 @@
-# Extract files and place them in the pynsist_pkgs folder
+# Extracts all dependencies and places them in the pynsist_pkgs folder
 
 mkdir pynsist_pkgs
 
@@ -18,86 +18,20 @@ rm -r numpy
 cp -r bindings/* pynsist_pkgs
 rm -r bindings
 
-# ATK
-7z x pygi/noarch/ATK/ATK.data.7z -oATKnoarch
-cp -r ATKnoarch/gnome/* pynsist_pkgs/gnome
-rm -r ATKnoarch
+# Copy the noarch and specified architecture dependencies into the gnome folder
+array=( ATK Base GDK GDKPixbuf GTK JPEG Pango WebP TIFF )
 
-7z x pygi/rtvc10-64/ATK/ATK.bin.7z -oATK
-cp -r ATK/gnome/* pynsist_pkgs/gnome
-rm -r ATK
+for i in "${array[@]}"
+do
+    echo -e "\nProcessing $i dependency"
+    7z x pygi/noarch/$i/$i.data.7z -o$i-noarch
+    cp -r $i-noarch/gnome/* pynsist_pkgs/gnome
+    rm -r $i-noarch
 
-# Base
-7z x pygi/noarch/Base/Base.data.7z -oBasenoarch
-cp -r Basenoarch/gnome/* pynsist_pkgs/gnome
-rm -r Basenoarch
-
-7z x pygi/rtvc10-64/Base/Base.bin.7z -oBase
-cp -r Base/gnome/* pynsist_pkgs/gnome
-rm -r Base
-
-# GDK
-7z x pygi/noarch/GDK/GDK.data.7z -oGDKnoarch
-cp -r GDKnoarch/gnome/* pynsist_pkgs/gnome
-rm -r GDKnoarch
-
-7z x pygi/rtvc10-64/GDK/GDK.bin.7z -oGDK
-cp -r GDK/gnome/* pynsist_pkgs/gnome
-rm -r GDK
-
-# GDKPixbuf
-7z x pygi/noarch/GDKPixbuf/GDKPixbuf.data.7z -oGDKPixbufnoarch
-cp -r GDKPixbufnoarch/gnome/* pynsist_pkgs/gnome
-rm -r GDKPixbufnoarch
-
-7z x pygi/rtvc10-64/GDKPixbuf/GDKPixbuf.bin.7z -oGDKPixbuf
-cp -r GDKPixbuf/gnome/* pynsist_pkgs/gnome
-rm -r GDKPixbuf
-
-# GTK
-7z x pygi/noarch/GTK/GTK.data.7z -oGTKnoarch
-cp -r GTKnoarch/gnome/* pynsist_pkgs/gnome
-rm -r GTKnoarch
-
-7z x pygi/rtvc10-64/GTK/GTK.bin.7z -oGTK
-cp -r GTK/gnome/* pynsist_pkgs/gnome
-rm -r GTK
-
-# JPEG
-7z x pygi/noarch/JPEG/JPEG.data.7z -oJPEGnoarch
-cp -r JPEGnoarch/gnome/* pynsist_pkgs/gnome
-rm -r JPEGnoarch
-
-7z x pygi/rtvc10-64/JPEG/JPEG.bin.7z -oJPEG
-cp -r JPEG/gnome/* pynsist_pkgs/gnome
-rm -r JPEG
-
-# Pango
-7z x pygi/noarch/Pango/Pango.data.7z -oPangonoarch
-cp -r Pangonoarch/gnome/* pynsist_pkgs/gnome
-rm -r Pangonoarch
-
-7z x pygi/rtvc10-64/Pango/Pango.bin.7z -oPango
-cp -r Pango/gnome/* pynsist_pkgs/gnome
-rm -r Pango
-
-# WebP
-7z x pygi/noarch/WebP/WebP.data.7z -oWebPnoarch
-cp -r WebPnoarch/gnome/* pynsist_pkgs/gnome
-rm -r WebPnoarch
-
-7z x pygi/rtvc10-64/WebP/WebP.bin.7z -oWebP
-cp -r WebP/gnome/* pynsist_pkgs/gnome
-rm -r WebP
-
-# TIFF
-7z x pygi/noarch/TIFF/TIFF.data.7z -oTIFFnoarch
-cp -r TIFFnoarch/gnome/* pynsist_pkgs/gnome
-rm -r TIFFnoarch
-
-7z x pygi/rtvc10-64/TIFF/TIFF.bin.7z -oTIFF
-cp -r TIFF/gnome/* pynsist_pkgs/gnome
-rm -r TIFF
+    7z x pygi/rtvc10-64/$i/$i.bin.7z -o$i-arch
+    cp -r $i-arch/gnome/* pynsist_pkgs/gnome
+    rm -r $i-arch
+done
 
 #Remove pygi Folder
 rm -r pygi
