0a8b799cea388804391884839e7b8b5c4c3dd4bd
Thomas Kluyver <thomas@kluyver.me.uk>
2016-07-23 19:44:36 +0100

    Allow a Python version specific ABI tag, like cp35m

--- nsist/pypi.py
+++ nsist/pypi.py
@@ -37,8 +37,10 @@ class WheelDownloader(object):
         return max(d.get(p, 0) for p in platform.split('.'))
 
     def score_abi(self, abi):
+        py_version_nodot = ''.join(self.py_version.split('.')[:2])
         # Are there other valid options here?
-        d = {'abi3': 2, 'none': 1}
+        d = {'cp%sm' % py_version_nodot: 3,  # Is the m reliable?
+            'abi3': 2, 'none': 1}
         return max(d.get(a, 0) for a in abi.split('.'))
 
     def score_interpreter(self, interpreter):
