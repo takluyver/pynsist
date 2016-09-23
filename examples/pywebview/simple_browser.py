91a35d2ed4539bfab77b9df4f80c228c750c5148
Thomas Kluyver <thomas@kluyver.me.uk>
2016-09-23 14:02:51 +0100

    Change URL in pywebview example

--- examples/pywebview/simple_browser.py
+++ examples/pywebview/simple_browser.py
@@ -9,7 +9,7 @@ import webview
 
 def main():
     # Create a non-resizable webview window with 800x600 dimensions
-    webview.create_window("Simple browser", "http://www.flowrl.com",
+    webview.create_window("Simple browser", "http://pynsist.readthedocs.io/",
                             width=800, height=600, resizable=False)
 
 if __name__ == '__main__':
