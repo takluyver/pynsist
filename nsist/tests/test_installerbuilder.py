4a7f1048b2c595e2c9cb0cfff31c5b06fec7cffe
Thomas Kluyver <thomas@kluyver.me.uk>
2018-09-30 11:30:47 +0200

    Add failing test for clashing filenames

--- nsist/tests/test_installerbuilder.py
+++ nsist/tests/test_installerbuilder.py
@@ -1,4 +1,5 @@
 import io
+import os
 from os.path import join as pjoin
 import shutil
 import tempfile
@@ -39,3 +40,27 @@ class TestInstallerBuilder(unittest.TestCase):
             preamble_contents = f.read().strip()
         
         assert preamble_contents in contents
+
+    def test_copy_files(self):
+        files = [
+            (pjoin(test_dir, 'data_files', 'dir1', 'eg-data.txt'), '$INSTDIR'),
+            (pjoin(test_dir, 'data_files', 'dir2', 'eg-data.txt'), '$INSTDIR\\foo'),
+            (pjoin(test_dir, 'data_files', 'dir1', 'subdir'), '$INSTDIR'),
+            (pjoin(test_dir, 'data_files', 'dir2', 'subdir'), '$INSTDIR\\foo'),
+        ]
+        ib = InstallerBuilder("Test App", "1.0", {}, extra_files=files,
+                              build_dir=self.target)
+        ib.copy_extra_files()
+
+        build_dir_files = set(os.listdir(self.target))
+        self.assertEqual(build_dir_files,
+                         {'eg-data.txt', 'eg-data.1.txt', 'subdir', 'subdir.1'})
+
+        self.assertEqual(ib.install_dirs, [
+            ('subdir', '$INSTDIR'),
+            ('subdir.1', '$INSTDIR\\foo'),
+        ])
+        self.assertEqual(ib.install_files, [
+            ('eg-data.txt', '$INSTDIR'),
+            ('eg-data.1.txt', '$INSTDIR\\foo'),
+        ])
